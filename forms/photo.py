6dd0f1cc51291ebc4e8f637efd074ad495a21720
kepchup228 <164914727+kepchup228@users.noreply.github.com>
2025-05-12 23:44:44 +0300

    good

--- forms/photo.py
+++ forms/photo.py
@@ -3,14 +3,14 @@ from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, TextAreaField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
-
+# Форма для загрузки фотографии
 class PhotoForm(FlaskForm):
     title = StringField('Название', validators=[DataRequired()])
     description = TextAreaField("Описание")
     tags = StringField('Теги (через запятую)')
     photo = FileField('Фотография', validators=[
-        FileRequired(),
-        FileAllowed(['jpg', 'jpeg', 'png'], 'Только изображения!')
+        FileRequired(),  # Обязательное поле
+        FileAllowed(['jpg', 'jpeg', 'png'], 'Только изображения!')  # Форматы файлов
     ])
     is_private = BooleanField("Приватное")
     submit = SubmitField('Загрузить')

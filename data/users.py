eee82b3ffeeec252dfafd601834d7d73abc8dd4f
kepchup228 <164914727+kepchup228@users.noreply.github.com>
2025-05-12 23:47:12 +0300

    good

--- data/users.py
+++ data/users.py
@@ -6,24 +6,24 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from .db_session import SqlAlchemyBase
 
-
+# Пользователь и методы для работы с паролем
 class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)
-    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    email = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=True)  # Электронная почта
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)  # Хэш пароля
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)  # Дата
 
-    photos = orm.relationship("Photo", back_populates="user")
+    photos = orm.relationship("Photo", back_populates="user")  # Связь с фотографиями
 
     def __repr__(self):
-        return f'<User> {self.id} {self.name} {self.email}'
+        return f'<User> {self.id} {self.name} {self.email}'  # Строкой
 
     def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
+        self.hashed_password = generate_password_hash(password)  # Хэш пароля
 
     def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+        return check_password_hash(self.hashed_password, password)  # Проверка пароля

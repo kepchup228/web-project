dd1ec1025f49a6e3d2c556aec710bc9f007aa2b6
kepchup228 <164914727+kepchup228@users.noreply.github.com>
2025-05-12 18:28:28 +0300

    rabotaet

--- data/photos.py
+++ data/photos.py
@@ -1,19 +1,19 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
-
 from .db_session import SqlAlchemyBase
 
 
-class News(SqlAlchemyBase):
-    __tablename__ = 'news'
+class Photo(SqlAlchemyBase):
+    __tablename__ = 'photos'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    filename = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     created_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     is_private = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
 
     user_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
-    user = orm.relation('User')
-    categories = orm.relation("Category", secondary="association", backref="news")
+    user = orm.relationship('User', back_populates="photos")
+    tags = orm.relationship("Tag", secondary="association", back_populates="photos")
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, redirect, request, abort
+from flask import Flask, render_template, redirect, request, abort, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import os
+from werkzeug.utils import secure_filename
 
-from forms.news import NewsForm
+from forms.photo import PhotoForm
 from forms.user import RegisterForm, LoginForm
-from data.news import News
+from data.photos import Photo
+from data.tags import Tag
 from data.users import User
 from data import db_session
 
@@ -11,6 +14,8 @@ app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'png'}
 
 
 @login_manager.user_loader
@@ -26,80 +31,160 @@ def logout():
     return redirect("/")
 
 
-def main():
-    db_session.global_init("db/blogs.db")
-    app.run()
-
-
-@app.route('/news', methods=['GET', 'POST'])
+@app.route('/add_photo', methods=['GET', 'POST'])
 @login_required
-def add_news():
-    form = NewsForm()
+def add_photo():
+    form = PhotoForm()
     if form.validate_on_submit():
         db_sess = db_session.create_session()
-        news = News()
-        news.title = form.title.data
-        news.content = form.content.data
-        news.is_private = form.is_private.data
-        current_user.news.append(news)
-        db_sess.merge(current_user)
-        db_sess.commit()
-        return redirect('/')
-    return render_template('news.html', title='Добавление новости', form=form)
+
+        try:
+            # Получаем пользователя из текущей сессии
+            user = db_sess.query(User).get(current_user.id)
+
+            # Сохраняем файл
+            file = form.photo.data
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
+
+                # Создаем запись о фото
+                photo = Photo(
+                    title=form.title.data,
+                    description=form.description.data,
+                    filename=filename,
+                    is_private=form.is_private.data,
+                    user=user  # Связываем с пользователем из сессии
+                )
+
+                # Обрабатываем теги
+                tags = [t.strip() for t in form.tags.data.split(',') if t.strip()]
+                for tag_name in tags:
+                    tag = db_sess.query(Tag).filter(Tag.name == tag_name.lower()).first()
+                    if not tag:
+                        tag = Tag(name=tag_name.lower())
+                        db_sess.add(tag)
+                    photo.tags.append(tag)
+
+                db_sess.add(photo)
+                db_sess.commit()
+                return redirect('/')
+            else:
+                return render_template('photo.html',
+                                       title='Добавление фотографии',
+                                       form=form,
+                                       message="Недопустимый формат файла")
+        finally:
+            db_sess.close()
+    return render_template('photo.html', title='Добавление фотографии', form=form)
 
 
-@app.route('/news_delete/<int:id>', methods=['GET', 'POST'])
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.route('/edit_photo/<int:id>', methods=['GET', 'POST'])
 @login_required
-def news_delete(id):
+def edit_photo(id):
+    form = PhotoForm()
+    if request.method == "GET":
+        db_sess = db_session.create_session()
+        photo = db_sess.query(Photo).filter(Photo.id == id, Photo.user == current_user).first()
+        if photo:
+            form.title.data = photo.title
+            form.description.data = photo.description
+            form.tags.data = ', '.join([tag.name for tag in photo.tags])
+            form.is_private.data = photo.is_private
+        else:
+            abort(404)
+
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        photo = db_sess.query(Photo).filter(Photo.id == id, Photo.user == current_user).first()
+        if photo:
+            photo.title = form.title.data
+            photo.description = form.description.data
+            photo.is_private = form.is_private.data
+
+            photo.tags = []
+            tags = [t.strip() for t in form.tags.data.split(',') if t.strip()]
+            for tag_name in tags:
+                tag = db_sess.query(Tag).filter(Tag.name == tag_name.lower()).first()
+                if not tag:
+                    tag = Tag(name=tag_name.lower())
+                    db_sess.add(tag)
+                photo.tags.append(tag)
+
+            file = form.photo.data
+            if file and allowed_file(file.filename):
+                try:
+                    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], photo.filename))
+                except:
+                    pass
+
+                filename = secure_filename(file.filename)
+                filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                file.save(filepath)
+                photo.filename = filename
+
+            db_sess.commit()
+            return redirect('/')
+        else:
+            abort(404)
+    return render_template('photo.html', title='Редактирование фотографии', form=form)
+
+
+@app.route('/delete_photo/<int:id>', methods=['GET', 'POST'])
+@login_required
+def delete_photo(id):
     db_sess = db_session.create_session()
-    news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
-    if news:
-        db_sess.delete(news)
+    photo = db_sess.query(Photo).filter(Photo.id == id, Photo.user == current_user).first()
+    if photo:
+        try:
+            os.remove(os.path.join(app.config['UPLOAD_FOLDER'], photo.filename))
+        except:
+            pass
+
+        db_sess.delete(photo)
         db_sess.commit()
     else:
         abort(404)
     return redirect('/')
 
 
-@app.route('/news/<int:id>', methods=['GET', 'POST'])
-@login_required
-def edit_news(id):
-    form = NewsForm()
-    if request.method == "GET":
-        db_sess = db_session.create_session()
-        news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
-        if news:
-            form.title.data = news.title
-            form.content.data = news.content
-            form.is_private.data = news.is_private
-        else:
-            abort(404)
-    if form.validate_on_submit():
-        db_sess = db_session.create_session()
-        news = db_sess.query(News).filter(News.id == id, News.user == current_user).first()
-        if news:
-            news.title = form.title.data
-            news.content = form.content.data
-            news.is_private = form.is_private.data
-            db_sess.commit()
-            return redirect('/')
-        else:
-            abort(404)
-    return render_template('news.html', title='Редактирование новости', form=form)
+@app.route('/search')
+def search():
+    query = request.args.get('q', '').strip().lower()
+    if not query:
+        return redirect(url_for('index'))
+
+    db_sess = db_session.create_session()
+    photos = db_sess.query(Photo).join(Photo.tags).filter(Tag.name.contains(query))
+
+    if not current_user.is_authenticated:
+        photos = photos.filter(Photo.is_private != True)
+    elif current_user.is_authenticated:
+        photos = photos.filter((Photo.user == current_user) | (Photo.is_private != True))
+
+    photos = photos.all()
+    return render_template('index.html', photos=photos, search_query=query)
 
 
 @app.route("/")
 def index():
     db_sess = db_session.create_session()
     if current_user.is_authenticated:
-        news = db_sess.query(News).filter((News.user == current_user) | (News.is_private != True))
+        photos = db_sess.query(Photo).filter((Photo.user == current_user) | (Photo.is_private != True))
     else:
-        news = db_sess.query(News).filter(News.is_private != True)
-    return render_template("index.html", news=news)
+        photos = db_sess.query(Photo).filter(Photo.is_private != True)
+    photos = photos.order_by(Photo.created_date.desc()).all()
+    return render_template("index.html", photos=photos)
 
 
 @app.route('/register', methods=['GET', 'POST'])
-def reqister():
+def register():
     form = RegisterForm()
     if form.validate_on_submit():
         if form.password.data != form.password_again.data:
@@ -132,6 +217,13 @@ def login():
             return redirect("/")
         return render_template('login.html', message="Неправильный логин или пароль", form=form)
     return render_template('login.html', title='Авторизация', form=form)
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
